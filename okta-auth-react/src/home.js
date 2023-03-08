b3896910e4140513e0959ea5f186fc1a8a369f76
parisadikshit <2018bec034@sggs.ac.in>
2023-03-08 15:11:44 +0530

    Changes for the logout method

--- okta-auth-react/src/home.js
+++ okta-auth-react/src/home.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useOktaAuth } from "@okta/okta-react";
+import { Menu } from "antd";
+import {
+    AppstoreOutlined,
+    LoginOutlined,
+  } from "@ant-design/icons";
+
+const { Item } = Menu;
 export const Home = () =>{
     const {oktaAuth} = useOktaAuth();
     const [user, setUser] = useState();
@@ -14,12 +21,32 @@ export const Home = () =>{
     }
     useEffect(()=>{
        const user =  getUser();
+       console.log(user)
        
     },[])
     return (
         <>
-        <pre>{JSON.stringify(user, null, 5)}</pre>
-        <button onClick={logout}>logout</button>
+        <Menu mode="horizontal" >
+        <Item
+            key="/home"
+            icon={<AppstoreOutlined />}
+        >
+            App
+        </Item>
+      
+         
+        <Item
+            key="/"
+            onClick={logout}
+            icon={<LoginOutlined />}
+            >
+            Logout
+            </Item>
+   
+         
+        
+      </Menu>
+      <pre>{JSON.stringify(user, null, 5)}</pre>
         </>
     )
 }
